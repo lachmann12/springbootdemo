7390c172ea1c7e255d748bbcc54f10978141c46f
Alexander Lachmann <32603869+lachmann12@users.noreply.github.com>
2021-03-01 00:33:52 -0500

    Update buildDocker.sh

--- demo/docker/buildDocker.sh
+++ demo/docker/buildDocker.sh
@@ -1,6 +1,8 @@
-cd..
+cd ..
 mvnw clean install
 cd docker
 
-docker build -f Dockerfile -t lachmann12/springbootdemo .
+chmod 777 ../target/demo-0.0.1-SNAPSHOT.war
+
+docker build -f DockerFile -t lachmann12/springbootdemo .
 #docker push lachmann12/springbootdemo
